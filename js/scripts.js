68f61427f8ea3a04326d5381a044a026b9e9a016
Student <example@example.com>
2019-04-15 16:31:13 -0700

    Create card constructor that can call on HTML

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,31 @@ function Card (index, image, fortune) {
   this.fortune = fortune;
 }
 
-  for (var i=0; i<20; i++) {
+  for (var i=1; i<21; i++) {
     deck.push(new Card(i, "a", "b"));
   };
+
+let card1 = new Card(0, "img", "fortune")
+
+var chosenCards = (firstCard, secondCard, thirdCard)
+
+function selectCard{
+  index = math.random
+  return deck[index]
+  chosenCards.push()
+}
+
+// Creating new cards dynamically
+card {
+  this.image = "img#"
+}
+
+card2.index = 2
+card2[image] = card[image+card2.index] =
+card2{
+  image: "img#2"
+}
+
+<img id="2">
+
+"img#2"
