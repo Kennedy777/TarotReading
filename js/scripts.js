62365af52ed865808531c20702dbddda60630349
Student <example@example.com>
2019-04-16 10:26:44 -0700

    Add toggle function to switch the Tarot Cards

--- js/scripts.js
+++ js/scripts.js
@@ -1,62 +1,22 @@
+//back end Logic
+
+
+
+
+
 
 // User Logic
-
 $(document).ready(function() {
 
 
-  $("#fade-in").click(function() {
-    $("img1").fadeIn("fast");
-  });
-
-  $("#fade-out").click(function() {
-    $("img1").fadeOut(400);
-  });
-
   $("#toggle-fading").click(function() {
-    $('#img1').fadeToggle("slow");
+    $(this).find("img").toggle(200);
   });
   $("#toggle-fading2").click(function() {
-    $('#img2').fadeToggle("slow");
+    $(this).find("img").toggle(200);
   });
-
   $("#toggle-fading3").click(function() {
-    $('#img3').fadeToggle("slow");
+    $(this).find("img").toggle(200);
   });
-
-
-
-
+    event.preventDefault();
 });
-
-
-
-
-
-
-
-
-
-
-
-
-// $(document).ready(function() {
-//   $(".clickable").click(function() {
-//
-//
-//
-//     $("#toggle-fading").click(function() {
-//       $('img').fadeToggle("slow");
-//     });
-//
-//
-//
-// var imgArray = [];
-// imgArray[0] = new Image ();
-// imgArray[0].src = "img/backCard.png";
-//
-//
-//
-//
-//   event.preventDefault();
-//   });
-// });
