2e79013d90c269d9336b49dc14c474eae151794b
Student <example@example.com>
2019-04-16 09:53:24 -0700

    function that randomly selects card

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,9 @@
+//Empty deck of cards
 var deck = [];
+
+for (var i=1; i<21; i++) {
+  deck.push(new Card(i, "img#"+i, "fortune#"+i));
+};
 
 function Card (id, image, fortune) {
   this.id = id;
@@ -6,31 +11,13 @@ function Card (id, image, fortune) {
   this.fortune = fortune;
 }
 
-  for (var i=1; i<21; i++) {
-    deck.push(new Card(i, "img#"+i, "fortune#"+i));
-  };
 
-let card1 = new Card(0, "img", "fortune")
 
-var chosenCards = (firstCard, secondCard, thirdCard)
 
-function selectCard{
-  index = math.random
-  return deck[index]
-  chosenCards.push()
-}
 
 // Creating new cards dynamically
-card {
-  this.image = "img#2"
-}
 
-card2.index = 2
-card2[image] = card[image+card2.index] =
-card2{
-  image: "img#2"
-}
-
-<img id="2">
-
-"img#2"
+function selectCard(){
+var cardNumber = Math.floor((Math.random() * 20) + 1 );
+return deck[cardNumber];
+};
