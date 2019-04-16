ac9990addb0a60bcbf950695cad4e6b97d3c1d07
Student <example@example.com>
2019-04-16 13:27:16 -0700

    Add picture, refresh button and find the final function to show the tarot reading picture

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,24 @@
-//back end Logic
+//Back end Logic
 
+// Creation of the Constructor for the list
 
+// Prototype fo radd to an array every
+
+//Creation of the Constructor of the Game
+
+var array1= [""]
 
 
 
 
 // User Logic
 $(document).ready(function() {
+  var justice = "img/temperance.png"
+  var game = true;
+  var taille = "350px";
+
+  $("#imgSA").html("<img src=" + justice + " height="+ taille +">");
+
 
 
   $("#toggle-fading").click(function() {
@@ -16,7 +28,11 @@ $(document).ready(function() {
     $(this).find("img").toggle(200);
   });
   $("#toggle-fading3").click(function() {
-    $(this).find("img").toggle(200);
-  });
-    event.preventDefault();
+    $(this).find("#img3").fadeOut(100);
+    $("#imgSA").show();
+
+   });
+
+
+event.preventDefault();
 });
